5bca81b6362265c65f34a4f91d9826e02727f9f5
piyushgarg362-commits <piyushgarg362@gmail.com>
2026-09-14 21:05:29 -0400

    Create App.java

---
+++ App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Java Maven Docker Jenkins!");
+    }
+
+    public static String message() {
+        return "Hello from Java Maven Docker Jenkins!";
+    }
+}
